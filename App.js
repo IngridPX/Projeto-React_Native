2a1a2b17bae1e72ef3d4282f1da5807f7c8348a4
IngridPX <72896995+IngridPX@users.noreply.github.com>
2021-02-20 02:50:13 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './components/login'
+import Home from './components/home'
+import Bolos from './components/bolos'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer initialRouteName='Login' headerMode='screen' >
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Tela de Login' }} />
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+        <Stack.Screen name="Bolos" component={Bolos} options={{ title: 'Bolos' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
